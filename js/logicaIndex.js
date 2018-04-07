c33aad1233515355e52f1c1ec3b090c2151a639f
Samir Mejia <samir4mt@gmail.com>
2018-04-07 15:42:29 -0500

    Initial commit

---
+++ js/logicaIndex.js
@@ -0,0 +1,46 @@
+$(function(){
+	$('#card-login').fadeIn(1500);
+
+	$('#btnLogin').click(function(){
+		var cedula = $('#cedula').val();
+		var contrasena = $('#contrasena').val();
+		var seEncontro = false;
+		
+		$.each(json.usuarios, function(indice, elemento){
+			if (elemento.cedula == cedula){
+				seEncontro = true;
+				if (elemento.contrasena == contrasena){
+
+					if (elemento.tipo == "jurado"){
+						location.href = "paginas/vista-jurado.html";
+					}
+					if (elemento.tipo == "votante"){
+						location.href = "paginas/vista-votante.html";
+					}
+					return;
+				}else{
+					mostrarModal("Contraseña incorrecta", "La contraseña ingresada es incorrecta.");
+				}
+			}
+		});
+		if (!seEncontro) {
+			mostrarModal("Usuario no encontrado", "No existe un usuario con este número cedula");
+		}
+
+	});
+
+	$('a#recordarDatos').click(function() {
+		var pMensajeModal = $('#mensaje-modal');
+		var lista = JSON.stringify(json.usuarios);
+		mostrarModal("Usuarios Registrados", lista);
+	});
+
+});
+
+$('.modal').modal();
+
+function mostrarModal(titulo, mensaje){
+	$('#titulo-modal').text(titulo);
+	$('#mensaje-modal').text(mensaje);
+	$('#myModal').modal('open');
+}
---
+++ json/usuarios.js
@@ -0,0 +1,25 @@
+var json = {
+	usuarios :[
+		{
+			cedula : "12345600",
+			contrasena : "1234",
+			tipo : "votante"
+		},
+		{
+			cedula : "11110000",
+			contrasena : "1100",
+			tipo : "votante"
+		},
+		{
+			cedula : "11112222",
+			contrasena : "1122",
+			tipo : "jurado"
+		},
+		{
+			cedula : "21212121",
+			contrasena : "2121",
+			tipo : 'jurado'
+		}
+
+	]
+}
